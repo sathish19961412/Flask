251b1e92647188a2c09ae27bc1d7f1a994f3afa9
sathish19961412 <sathishfreelanc5@gmail.com>
2023-04-22 11:25:16 +0530

    Basic Route add

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from  flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return '<h2>Welcome TO The sathish</h2>'
+    return '<h2>Welcome TO The sathish</h2><br><h3>Learn More We Smart</h3>'
+@app.route('/about')
+def about():
+    return '<h2>Welcome To The About</h3>'
+
+@app.route('/contact')
+def contact():
+    return '<h2>Contact US</h3>'
 
 if __name__=='__main__':
     app.run(debug=True)
