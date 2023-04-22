a49af1ffd5cfaba16e76c8b36e326e31a28b898a
sathish19961412 <sathishfreelanc5@gmail.com>
2023-04-22 11:38:44 +0530

    Basic Dynamic Route add

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def about():
 def contact():
     return '<h2>Contact US</h3>'
 
+#Dynamic Routing
+@app.route('/users/<name>')
+def users(name):
+    return "<h3>Welcome To {}</h3>".format(name)
+
 if __name__=='__main__':
     app.run(debug=True)
 
